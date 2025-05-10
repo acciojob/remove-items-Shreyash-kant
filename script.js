a450c0aaccc9e9900d2cfd4de4851e5df379d854
Shreyash Kant <108665847+Shreyash-kant@users.noreply.github.com>
2025-05-10 14:54:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const selection  = document.getElementById("colorSelect");
+const btn = document.getElementsByTagName("input")[0];
+btn.addEventListener("click",()=>{
+	const selectIndex = selection.selectedIndex;
+	if(selectIndex!==-1){
+		selection.remove(selectIndex);
+	}
+})
